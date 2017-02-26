4257c9fb5c2f85954f7a3626b62a4a65d7ec39bb
peterMac <foo@bar.deadbeef>
2017-02-26 11:06:00 +0800

    foo

--- HW1/SubFactorial.java
+++ HW1/SubFactorial.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
-class SubFactorial
+class Functional
 {
     public static int Subfact(int n) {
         int res = n!=0?n*Subfact(n-1)+(n%2==0?1:-1):1;
         System.out.println("!"+n+"="+res);
         return res;
     }
+};
 
+class SubFactorial
+{
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
+        Functional SF = new Functional();
         int n;
         String test;
         do {
@@ -18,7 +22,7 @@ class SubFactorial
             scanner.nextLine(); //This is needed to pick up the new line
             if(n>12) System.out.println("N is too big ("+n+">12)...");
             else if(n<0) System.out.println("N is too small ("+n+"<0)...");
-            else Subfact(n);
+            else SF.Subfact(n);
             System.out.print("Enter Y/y to continue or enter any key to exit... ");
             test=scanner.nextLine();
         } while(test.equalsIgnoreCase("y"));

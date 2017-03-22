c17f81b813c2a49b561d44e8bdfc2abb024ac30a
peterMac <foo@bar.deadbeef>
2017-03-22 10:47:31 +0800

    foo

---
+++ HW2/include/global.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <stdexcept>
+#include <limits>
+using namespace std;
+typedef long long int UT;
+typedef list< list<UT> > LOL; // list-of-list
+
---
+++ HW2/main.cpp
@@ -0,0 +1,15 @@
+/*
+*   Author: 404410030 - Kuang-Yu Jeng
+*   2017/03/21
+*   All Rights Reserved
+*/
+
+#include "include/global.hpp"
+#include "include/Pascal.hpp"
+#include "include/UI.hpp"
+
+int main(void) {
+    UserInterface UI;
+    UI.pascalMainLoop();
+    return 0;
+}
